e93856e208a66b5764d3aefcd870865d8c5006b7
Raphael <rafa.ramos_22@hotmail.com>
2021-11-26 18:29:15 -0300

    comparaçao com 3 constantes

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -6,10 +6,18 @@ let b = 6
  console.log(a*b);
  console.log(a%b);
  const c = 10;
- const d = 15
- 
+ const d = 15;
+ const e = 20;
  if(c > d){
     console.log(c)
  }else{
      console.log(d)
+ }
+ 
+ if(c > d && c > e){
+    console.log(c)
+ }else if(d > c && d > e){
+     console.log(d)
+ }else{
+     console.log(e)
  }
