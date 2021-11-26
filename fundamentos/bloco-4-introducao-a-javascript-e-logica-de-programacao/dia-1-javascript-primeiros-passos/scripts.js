1bcc59c2c6f00acffa5fde43757d6a4b3512ace5
Raphael <rafa.ramos_22@hotmail.com>
2021-11-26 18:36:01 -0300

    verificando se o numero e positivo, ngativo ou zero

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,3 +1,4 @@
+// Exercício 1
 let a = 5
 let b = 6
  console.log(a+b);
@@ -5,15 +6,17 @@ let b = 6
  console.log(a/b);
  console.log(a*b);
  console.log(a%b);
+//  Exercício 2
  const c = 10;
  const d = 15;
  const e = 20;
+ 
  if(c > d){
     console.log(c)
  }else{
      console.log(d)
  }
- 
+//  Exercício 3
  if(c > d && c > e){
     console.log(c)
  }else if(d > c && d > e){
@@ -21,3 +24,19 @@ let b = 6
  }else{
      console.log(e)
  }
+// Exercício 4
+if(c > 0){ 
+    console.log('positivo')
+}else if( c < 0){
+    console.log ('negatio')
+}else{
+    console.log('zero')
+}
+// Exercício 5
+// Exercício 6
+// Exercício 7
+// Exercício 8
+// Exercício 9
+// Exercício 10
+// Exercício 11
+
