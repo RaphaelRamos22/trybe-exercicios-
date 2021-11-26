015926e70fa3e512037cdc360a5f735aaae26170
Raphael <rafa.ramos_22@hotmail.com>
2021-11-26 18:20:10 -0300

    comparaçao com duas constantes

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,8 +1,15 @@
 let a = 5
 let b = 6
- console.log(a+b)
- console.log(a-b)
- console.log(a/b)
- console.log(a*b)
- console.log(a%b)
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a/b);
+ console.log(a*b);
+ console.log(a%b);
+ const c = 10;
+ const d = 15
  
+ if(c > d){
+    console.log(c)
+ }else{
+     console.log(d)
+ }
