906f5d3a7c771d9ebbb242361933f4ec75fbb587
Raphael <rafa.ramos_22@hotmail.com>
2022-06-09 19:46:58 -0300

    criando Api do simpsons

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const simpsonsUtils = require('./fs');
 const app = express();
+
 app.use(bodyParser.json());
-app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
-app.post('/hello', (req, res) => {
-  const { name } = req.body;
-  return res.status(200).json({ message: `Hello, ${name}!` });
+// // Crie uma rota GET /ping
+
+// app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
+
+// // Crie uma rota POST /hello
+// app.post('/hello', (req, res) => {
+//   const { name } = req.body;
+//   return res.status(200).json({ message: `Hello, ${name}!` });
+// });
+
+// Crie um endpoint GET /simpsons 🚀
+app.get('/simpsons', async (req, res) => {
+  try {
+    const simpsons = await simpsonsUtils.getSimpsons();
+
+    return res.status(200).json(simpsons);
+  } catch (error) {
+    return res.status(500).end();
+  }
 });
 
 app.listen(3000, () => console.log('ouvindo na porta 3000!'));
