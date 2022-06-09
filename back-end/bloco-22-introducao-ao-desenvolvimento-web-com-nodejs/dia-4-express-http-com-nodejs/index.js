b47e95a2688767dbd91c623278004220de0f5c6d
Raphael <rafa.ramos_22@hotmail.com>
2022-06-09 19:19:25 -0300

    criando a rota hello, recebendo do body da requesiçao JSON: { name:<nome do usuário> } e returnando o JSON: { message: Hello, <nome do usuário>! }

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -4,5 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  return res.status(200).json({ message: `Hello, ${name}!` });
+});
 
 app.listen(3000, () => console.log('ouvindo na porta 3000!'));
