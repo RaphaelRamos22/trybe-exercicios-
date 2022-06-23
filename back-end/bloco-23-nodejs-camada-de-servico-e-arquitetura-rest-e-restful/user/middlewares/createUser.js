a74220079a2a2aae13116f115b05e4e2e8562915
Raphael <rafa.ramos_22@hotmail.com>
2022-06-23 16:03:39 -0300

    feat: adicionando middlewares de validaçao de criaçao de usuario

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/user/middlewares/createUser.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+const rescue = require('express-rescue');
+const UserModel = require('../models/User')
+
+const userSchema = Joi.object({
+	firstName: Joi.string().required(),
+	lastName: Joi.string().required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().min(6).required()
+});
+ 
+const isValidCreateUser = (req, res, next) => {
+  const { error } = userSchema.validate(req.body);
+
+  if (error) return next(error);
+
+  next();
+}
+
+module.exports = isValidCreateUser
