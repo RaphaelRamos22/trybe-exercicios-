d9e8468e2996cf6aa6afae59fde90c655329d038
Raphael <rafa.ramos_22@hotmail.com>
2022-07-28 21:41:37 -0300

    feat: adicionando a interface IUser

--- back-end/bloco-26-typescript/typescript-express/src/models/connection.ts
+++ back-end/bloco-26-typescript/typescript-express/src/models/connection.ts
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
